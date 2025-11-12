3fdc58f04f667aa199fef6f96f973bf310853fec
Davyd19 <davydyehuda196@gmail.com>
2025-11-12 15:20:53 +0700

    feat: add is_admin column to users table for role management

--- src/migrations/20251108143113-create-all-tables.js
+++ src/migrations/20251108143113-create-all-tables.js
@@ -63,6 +63,11 @@ module.exports = {
         type: Sequelize.INTEGER,
         defaultValue: 0
       },
+      is_admin: { 
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false
+      },
       created_at: {
         type: Sequelize.DATE,
         allowNull: false,
@@ -377,18 +382,5 @@ module.exports = {
     await queryInterface.dropTable('badges');
     await queryInterface.dropTable('missions');
     await queryInterface.dropTable('users');
-  },
-
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('users', 'is_admin', {
-      type: Sequelize.BOOLEAN,
-      defaultValue: false,
-      allowNull: false
-    });
-  },
-
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('users', 'is_admin');
   }
-
 };
